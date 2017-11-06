8a6c22c41ae43c8812ad68a9ca22a05de4b1fcf3
Sander Vanhove <sandervhove@gmail.com>
2017-11-06 09:24:17 +0100

    Better standard values for lambda and sigma.

--- bin/event.js
+++ bin/event.js
@@ -5,33 +5,34 @@ const utils = require('./utils.js');
 
 class Event{
 	constructor(conf_reader){
-		this.starting_time = utils.getRandomNumber(0,23.5999);
+		this.starting_time = utils.getRandomNumber(0,24) % 24;
 
 		this.duration = utils.getRandomNumber((conf_reader.val("events:min_duration") || 0), (conf_reader.val("events:max_duration") || 1));
 
 		this.occupation = utils.getRandomNumber((conf_reader.val("events:min_occupation") || 0), (conf_reader.val("events:max_occupation") || 1));
 
-		console.log(this.starting_time,this.duration,this.occupation);
-
-
+		//Init values for lambda (Exponential) and sigma (Rayleigh)
 		this.min_lambda = conf_reader.val("events:min_lambda") || 0.5;
 		this.max_lambda = conf_reader.val("events:max_lambda") || 2;
-		this.lambda_var = conf_reader.val("events:lambda_var") || 0.5;
+		this.lambda_var = conf_reader.val("events:lambda_var") || 0.07;
 
 		this.min_sigma = conf_reader.val("events:min_sigma") || 0.05;
 		this.max_sigma = conf_reader.val("events:max_sigma") || 1;
-		this.sigma_var = conf_reader.val("events:sigma_var") || 0.4;
+		this.sigma_var = conf_reader.val("events:sigma_var") || 0.04;
 		
+		///Random starting values for lambda and sigma
 		this.lambda = utils.getRandomNumber(this.min_lambda, this.max_lambda);
 		this.sigma = utils.getRandomNumber(this.min_sigma, this.max_sigma);
 	}
 
 	/*
 		Take one step within the event
-		At the start of an event the amount of people should ramp up quickly (CDF Rayleigh) and at the end people leave slowly (PDF Exponantial).
+		At the start of an event the amount of people should ramp up quickly (CDF Rayleigh) and at the end people leave slowly (PDF Exponential).
 	*/
 	takeStep(x){
+		//Start time of event
 		let shifted_x = (x - this.starting_time) % 24;
+		//End time of event
 		let shifted_end_x = (shifted_x - this.duration) % 24;
 
 		let rate = 0;
